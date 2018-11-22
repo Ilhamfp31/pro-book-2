4aea0b5d6c2716bc864039e6d3fb27a22969f276
yusufrahmatp <yusufrahmatp@gmail.com>
2018-11-23 06:26:39 +0700

    Add response return if error occured so api caller doesn't wait forever, cleanup code

--- service/rest/api/controllers/validasi.js
+++ service/rest/api/controllers/validasi.js
@@ -3,10 +3,13 @@ var connection = require('../../conn');
 
 exports.validasi = function(req, res) {
     var no_kartu = req.body.no_kartu;
-    connection.query('SELECT id FROM nasabah WHERE no_kartu = '+no_kartu, function (error, rows, fields){
+    connection.query('SELECT id FROM nasabah WHERE no_kartu = '+ no_kartu, function (error, rows, fields) {
         if(error){
             console.log(error)
-        } else{
+            return res.status(500).json({
+                message : 'Internal server error'
+            });
+        } else {
             if (rows.length == 0) {
                 console.log("TIDAK VALID: "+no_kartu);
                 response.ok(0, res);
@@ -16,8 +19,4 @@ exports.validasi = function(req, res) {
             }
         }
     });
-};
-
-exports.index = function(req, res) {
-    response.ok("REST Web Service", res)
 };
