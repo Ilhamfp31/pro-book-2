e0ba1c659fba78821dd3650aef127d7e1d82917f
yusufrahmatp <yusufrahmatp@gmail.com>
2018-11-23 07:49:47 +0700

    Validasi rearranged using nasabah model query instead of directly connect to db

--- service/rest/api/controllers/validasi.js
+++ service/rest/api/controllers/validasi.js
@@ -1,28 +1,23 @@
 var response = require('../controllers/res');
-var connection = require('../models/conn');
+var nasabah = require('../models/nasabah');
 
 exports.validasi = function(req, res) {
     var no_kartu = req.body.no_kartu;
-    connection.query('SELECT id FROM nasabah WHERE no_kartu = '+ no_kartu, function (error, rows, fields) {
-        if(error){
-            console.log(error)
+    nasabah.getNasabahByCard(no_kartu, function(error, rows) {
+        if (error) {
+           console.log(error)
             return res.status(500).json({
                 message : 'Internal server error'
-            });
-        } else {
+            }); 
+        }
+        else {
             if (rows.length == 0) {
                 console.log("TIDAK VALID: "+no_kartu);
                 response.ok(0, res);
-                connection.destroy();
             } else {
                 console.log("VALID: "+no_kartu)
-                response.ok(1, res)
-                connection.destroy();
+                response.ok(1, res);
             }
         }
     });
-};
-
-exports.index = function(req, res) {
-    response.ok("REST Web Service", res);
 };
