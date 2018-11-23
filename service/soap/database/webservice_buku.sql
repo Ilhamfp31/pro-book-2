39ff35bffada6ae3b5168b6c9724fc489fd293ec
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-11-24 03:29:16 +0700

    add webservice buku database

---
+++ service/soap/database/webservice_buku.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.16  Distrib 10.3.10-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: webservice_buku
+-- ------------------------------------------------------
+-- Server version	10.3.10-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `webservice_buku`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `webservice_buku` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `webservice_buku`;
+
+--
+-- Table structure for table `daftar_harga`
+--
+
+DROP TABLE IF EXISTS `daftar_harga`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `daftar_harga` (
+  `id_buku` varchar(255) NOT NULL,
+  `harga` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_buku`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `daftar_harga`
+--
+
+LOCK TABLES `daftar_harga` WRITE;
+/*!40000 ALTER TABLE `daftar_harga` DISABLE KEYS */;
+/*!40000 ALTER TABLE `daftar_harga` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `daftar_penjualan`
+--
+
+DROP TABLE IF EXISTS `daftar_penjualan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `daftar_penjualan` (
+  `id_buku` varchar(255) NOT NULL,
+  `kategori` varchar(255) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_buku`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `daftar_penjualan`
+--
+
+LOCK TABLES `daftar_penjualan` WRITE;
+/*!40000 ALTER TABLE `daftar_penjualan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `daftar_penjualan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-24  3:29:00
