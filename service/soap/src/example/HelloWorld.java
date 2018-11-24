731596c307063a286f7e422176dbddc54c3c905e
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-11-25 01:58:05 +0700

    add example

--- service/soap/src/example/HelloWorld.java
+++ service/soap/src/example/HelloWorld.java
@@ -1,4 +1,6 @@
 package example;
+import model.DaftarHarga;
+import model.DaftarPenjualan;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import repository.BookRepository;
@@ -27,15 +29,33 @@ public class HelloWorld {
   public static void main(String[] argv) {
 
       // test getBookByTitle
-    ArrayList<Map<String,String>> testGetBookByTitle = getBookByTitle("kue");
-    System.out.println("\n\n Test balikan, Judul:" + testGetBookByTitle.get(0).get("title"));
+//    ArrayList<Map<String,String>> testGetBookByTitle = getBookByTitle("kue");
+//    System.out.println("\n\n Test balikan, Judul:" + testGetBookByTitle.get(0).get("title"));
 
     // test BookRepository
+    DaftarHarga dummyDaftarHarga1 = new DaftarHarga("napeDwAAQBAJ", 130000);
+    DaftarPenjualan dummyDaftarPejualan1 = new DaftarPenjualan("napeDwAAQBAJ", "Cooking", 3); // test daftar baru
+    DaftarPenjualan dummyDaftarPejualan2 = new DaftarPenjualan("Xl9nDwAAQBAJ", "Cooking", 1); // test daftar yang sudah ada
     BookRepository testBookRepository = new BookRepository();
     try {
       testBookRepository.connect();
+      DaftarHarga test1 = testBookRepository.getDaftarHarga("Xl9nDwAAQBAJ");
+      System.out.println("\nTest\nHarga buku: " + test1.getHarga());
 
-    } finally {
+      test1 = testBookRepository.getDaftarHarga("tubeswbdacac");
+      System.out.println("\nTest\nHarga buku: " + test1.getHarga());
+
+      DaftarPenjualan test2 = testBookRepository.getDaftarPenjualan("Xl9nDwAAQBAJ");
+      System.out.println("\nTest\nJumlah penjualan buku: " + test2.getJumlah());
+
+      testBookRepository.insertDaftarHarga(dummyDaftarHarga1);
+      testBookRepository.insertDaftarPenjualan(dummyDaftarPejualan1);
+      testBookRepository.insertDaftarPenjualan(dummyDaftarPejualan2);
+
+
+    } catch (SQLException e){
+      e.printStackTrace();
+    }finally {
       testBookRepository.disconnect();
     }
 
