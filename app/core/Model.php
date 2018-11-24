0349da01d07573fb97555bbbbe8de38fcec62b5e
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-11-25 04:43:22 +0700

    add datasource to intellij

--- app/core/Model.php
+++ app/core/Model.php
@@ -6,8 +6,8 @@ class Model
     public function __construct() {
         define('DB_SERVER', 'localhost');
         define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '072699');
-        define('DB_NAME', 'tubes1wbd');
+        define('DB_PASSWORD', 'wbd');
+        define('DB_NAME', 'wbdagung');
 
         $this->conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
