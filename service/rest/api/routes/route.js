5b07f0019197e01e3a2db2daad083454b506122c
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-11-28 02:52:19 +0700

    allow cors in rest webservice

--- service/rest/api/routes/route.js
+++ service/rest/api/routes/route.js
@@ -2,6 +2,12 @@ module.exports = function(app) {
     const validasi = require('../controllers/validasi');
     const nasabah = require('../controllers/transfer');
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.get('/', function(req, res) {
         res.json({
           'status': 200,
