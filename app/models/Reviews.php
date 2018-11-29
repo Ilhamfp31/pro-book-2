08aa121fe65cf6c5dd5935ac5b932d9bc3b3d656
priagungs <priagungsatyagama@gmail.com>
2018-11-29 20:32:57 +0700

    fix query syntax error in readAllReviewsByBookId

--- app/models/Reviews.php
+++ app/models/Reviews.php
@@ -6,7 +6,7 @@ class Reviews extends Model
     {
         $sql = "SELECT username, userPicture, rating, comment 
         FROM user INNER JOIN orders ON user.userID = orders.userID INNER JOIN review ON review.orderID = orders.orderID 
-        WHERE orders.bookID =" . $bookid;
+        WHERE orders.bookID ='" . $bookid . "'";
         $result = $this->conn->query($sql);
         while ($row = $result->fetch_assoc()) {
             $results[] = $row;
