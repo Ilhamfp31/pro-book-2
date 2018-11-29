0ead538e085f07adf72be14d5f740bafc074543e
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-11-30 00:35:45 +0700

    add token model

--- app/models/Token.php
+++ app/models/Token.php
@@ -2,10 +2,11 @@
 
 class Token extends Model
 {
-    function insertToken($id, $token, $browser, $ip_address){   
+    function insertToken($id, $token){   
         $sql = "DELETE FROM access_token WHERE id = '" . $id . "'";   
-        $this->conn->query($sql);
-
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        $this->conn->query($sql) ;
 
         $sql = "INSERT INTO access_token(id, token, browser, ip_address, time) VALUE("
                 .  "" . $id . ", "
@@ -17,12 +18,16 @@ class Token extends Model
         return $this->conn->query($sql);    
     }
 
-    function getIdByToken($token){
-
-    }
-
-    function validateToken(){
-
+    function validateToken($token){
+        $sql = "SELECT * FROM access_token WHERE token = '" . $token . "';" ;
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        $result = $this->conn->query($sql)->fetch_assoc();
+        if ($result['id'] && $result['browser'] == $browser && $result['ip_address'] == $ip_address && time() < (int)$result['time']) {
+            return $result['id'];
+        } else {
+            return null;
+        }
     }
 
     function deleteToken($token){
