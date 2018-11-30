479393eb08ede958fe64cc7a28a01fb7b24f0557
yusufrahmatp <yusufrahmatp@gmail.com>
2018-11-30 19:15:42 +0700

    Add review check in history view, fix history query, and fix review query

--- app/models/Order.php
+++ app/models/Order.php
@@ -4,27 +4,23 @@ class Order extends Model
 {
     function readHistoryByUserId($id)
     {
-        $sql = "SELECT * FROM
-                (SELECT date, quantity, bookPicture, title, reviewID, userID, orders.orderID
-                FROM orders INNER JOIN book ON orders.bookID = book.bookID
-                LEFT JOIN review ON review.orderID=orders.orderID
-                WHERE userid = '". $id ."') AS temp ORDER BY date DESC";
 
-        $sql = "SELECT orderID, bookID WHERE userID = " . $id . "ORDER BY id DESC";
+        $sql = "SELECT orderID, bookID FROM wbdprobook.orders WHERE userID = " . $id . " ORDER BY id DESC;";
         $result = $this->conn->query($sql);
         $results = [];
         while ($row = $result->fetch_assoc()) {
             $results[] = $row;
         }
+        require_once ('app/models/SoapHelper.php');
         $soap = new SoapHelper();
-        foreach ($results as $key -> $value) {
+        foreach ($results as $key => $value) {
+            $sql = "SELECT reviewID FROM wbdprobook.review WHERE orderID = '". $results[$key]['orderID'] ."';";
+            $results[$key]['reviewID'] = ($this->conn->query($sql) -> fetch_assoc());
             $data['book'] = $soap->getBookByID($results[$key]['bookID']);
             $data['order'] = $soap->getTransactionByID($results[$key]['orderID']);
             $results[$key]['date'] = $data['order']['timestamp'];
             $results[$key]['quantity'] = $data['order']['jumlah'];
             $results[$key]['bookPicture'] = $data['book']['bookPicture'];
-            //TODO REVIEW
-            $results[$key]['reviewID'] = '';
             $results[$key]['title'] = $data['book']['title'];
             $results[$key]['userID'] = $id;
         }
@@ -33,7 +29,7 @@ class Order extends Model
 
     function readReviewByOrderID($orderid)
     {
-        $sql = "SELECT bookID FROM orders WHERE orders.id='" . $orderid . "'";
+        $sql = "SELECT bookID FROM orders WHERE orders.orderID='" . $orderid . "'";
         $result = $this->conn->query($sql)->fetch_assoc();
         
         require_once ('app/models/SoapHelper.php');
